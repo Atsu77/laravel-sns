3e266a9c3e7fc8546568f0f9b9c7fced55bf6cc3
Atsu <kamnknon@gmail.com>
2022-03-05 11:28:42 +0900

    ユーザーの詳細ページを作成

--- resources/views/articles/card.blade.php
+++ resources/views/articles/card.blade.php
@@ -1,9 +1,13 @@
 <div class="card mt-3">
   <div class="card-body d-flex flex-row">
-    <i class="fas fa-user-circle fa-3x mr-1"></i>
+    <a href="{{ route('users.show', ['name' => $article->user->name])}}" class="text-dark">
+      <i class="fas fa-user-circle fa-3x mr-1"></i>
+    </a>
     <div>
       <div class="font-weight-bold">
-        {{ $article->user->name }}
+        <a href="{{ route('users.show', ['name' => $article->user->name])}}" class="text-dark">
+          {{ $article->user->name }}
+        </a>
       </div>
       <div class="font-weight-lighter">
         {{ $article->created_at->format('Y-m-d H:i') }}
@@ -71,12 +75,9 @@
   </div>
   <div class="card-body pt-0 pb-2 pl-3">
     <div class="card-text">
-      <article-like
-      :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
-      :initial-count-likes='@json($article->count_likes)'
-      :authorized='@json(Auth::check())'
-      endpoint="{{ route('articles.like', ['article' => $article]) }}"
-      >
+      <article-like :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
+        :initial-count-likes='@json($article->count_likes)' :authorized='@json(Auth::check())'
+        endpoint="{{ route('articles.like', ['article' => $article]) }}">
       </article-like>
     </div>
   </div>
